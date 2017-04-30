24d80c9c98a8131ad4762a60bb3525f41e498e64
loatheb <zz.innj@gmail.com>
2017-05-01 00:54:18 +0800

    feat(multi): add array support for params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const nativeModules = process.binding('natives')
+var nativeModules = process.binding('natives')
 
 module.exports = isNativeModules.isNativeModules = isNativeModules['default'] = isNativeModules
 
 function isNativeModules(name) {
-  return nativeModules.hasOwnProperty(name)
+  return Array.isArray(name) ? name.map(isNativeModules) : nativeModules.hasOwnProperty(name)
 }
